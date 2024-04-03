036efb61fb1ee86c2cd677b278497f7dcf8a8ca9
jorgeguedess <jorge_guedes00@hotmail.com>
2024-04-03 14:07:55 -0300

    feat: added repository interface

---
+++ src/repository/ProdutoRepository.ts
@@ -0,0 +1,10 @@
+import { Produto } from "../model/Produto";
+
+export interface ProdutoRepository {
+  // Métodos do CRbUD (Create, Read, Update, Delete)
+  listarTodos(): void;
+  buscarPorID(numero: number): void;
+  cadastrar(produto: Produto): void;
+  atualizar(produto: Produto): void;
+  deletar(id: number): void;
+}
